c8d37be7b4368b0833b9efa77512e7fcd8d51882
DudaNPM <duartenuno20111@hotmail.com>
2023-03-10 18:32:27 +0000

    write and save .ppm file

--- Image/ImagePPM.cpp
+++ Image/ImagePPM.cpp
@@ -24,12 +24,32 @@ void ImagePPM::ToneMap () {
 }
 
 bool ImagePPM::Save (std::string filename) {
-    
     // convert from float to {0,1,..., 255}
     ToneMap();
 
     // write imageToSave to file
+    std::ofstream ofs;
     
+    try {
+        ofs.open(filename, std::ios::binary); // need to spec. binary mode for Windows users
+        if (ofs.fail()) throw("Can't open output file");
+        ofs << "P6\n" << W << " " << H << "\n255\n";
+        unsigned char r, g, b;
+        for (int j = 0 ; j< H ; j++) {
+            for (int i = 0; i < W ; ++i) {
+                r = imageToSave[j*W+i].val[0];
+                g = imageToSave[j*W+i].val[1];
+                b = imageToSave[j*W+i].val[2];
+                ofs << r << g << b;
+            }
+        }
+        ofs.close();
+    }
+    catch (const char *err) {
+        fprintf(stderr, "%s\n", err);
+        ofs.close();
+    }
+
     // Details and code on PPM files available at:
     // https://www.scratchapixel.com/lessons/digital-imaging/simple-image-manipulations/reading-writing-images.html
     
